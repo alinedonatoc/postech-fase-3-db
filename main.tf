fdbbd1a865eda0b9028ca3441b7709783d61d347
aline.tanure <alinedonatoc@gmail.com>
2024-03-10 16:15:02 -0300

    Adjustment

--- main.tf
+++ main.tf
@@ -1,73 +1,77 @@
-locals {
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
 
-  postgres_identifier    = "pg_autoservico_id"
-  postgres_name          = "pg_autoservico_name"
-  postgres_user_name     = "test"
-  postgres_user_password = POSTGRES_USER_PASSWORD
-  postgres_instance_name = "pgautoservico_instance"
-  postgres_db_password   = POSTGRES_DB_PASSWORD
-  postgres_port          = 5432
-
-}
-
-// PROVIDERS
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
-provider "postgresql" {
-  host            = aws_db_instance.postgres.address
-  port            = local.postgres_port
-  database        = local.postgres_database_name
-  username        = local.postgres_username
-  password        = local.postgres_password
-  sslmode         = "require"
-  connect_timeout = 15
-  superuser       = false
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.77.0"
+
+  name                 = "education"
+  cidr                 = "10.0.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
-// POSTGRES
-resource "aws_security_group" "security_group_name" {
-  name = "security_group_name"
+resource "aws_db_subnet_group" "education" {
+  name       = "education"
+  subnet_ids = module.vpc.public_subnets
+
+  tags = {
+    Name = "Education"
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name   = "education_rds"
+  vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port   = local.postgres_port
-    to_port     = local.postgres_port
+    from_port   = 5432
+    to_port     = 5432
     protocol    = "tcp"
-    description = "PostgreSQL"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port        = local.postgres_port
-    to_port          = local.postgres_port
-    protocol         = "tcp"
-    description      = "PostgreSQL"
-    ipv6_cidr_blocks = ["::/0"]
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "education_rds"
   }
 }
 
-resource "aws_db_instance" "instance_name" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "12.2"
-  instance_class       = "db.t2.micro"
-  identifier           = local.postgres_identifier
-  name                 = local.postgres_instance_name
-  username             = local.postgres_user_name
-  password             = local.postgres_db_password
-  publicly_accessible  = true
-  parameter_group_name = "default.postgres12"
-  skip_final_snapshot  = true
+resource "aws_db_parameter_group" "education" {
+  name   = "education"
+  family = "postgres14"
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
 }
 
-resource "postgresql_role" "user_name" {
-  name                = local.postgres_user_name
-  login               = true
-  password            = local.postgres_user_password
-  encrypted_password  = true
-  create_database     = true
-  create_role         = true
-  skip_reassign_owned = true
+resource "aws_db_instance" "education" {
+  identifier             = "education"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "14.1"
+  username               = "edu"
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.education.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  parameter_group_name   = aws_db_parameter_group.education.name
+  publicly_accessible    = true
+  skip_final_snapshot    = true
 }
