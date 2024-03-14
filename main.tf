f86920f3b3613f6de4452d95786375f74c5d180d
aline.tanure <alinedonatoc@gmail.com>
2024-03-14 20:26:24 -0300

    Fix conflicts

--- main.tf
+++ main.tf
@@ -1,11 +1,49 @@
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  db_name              = "autoservico"
-  engine               = "postgres"
-  engine_version       = "14.11"
-  instance_class       = "db.t3.micro"
-  username             = "local"
-  password             = var.POSTGRES_DB_PASSWORD
-  parameter_group_name = "default.postgres16"
-  skip_final_snapshot  = true
+data "aws_availability_zones" "available" {}
+
+resource "aws_security_group" "rds" {
+  name   = "education_rds"
+  vpc_id = "education"
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "education_rds"
+  }
+}
+
+resource "aws_db_parameter_group" "education" {
+  name   = "education"
+  family = "postgres14"
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+}
+
+resource "aws_db_instance" "education" {
+  identifier             = "education"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "14.11"
+  username               = "edu"
+  password               = var.POSTGRES_DB_PASSWORD
+  db_subnet_group_name   = aws_db_subnet_group.education.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  parameter_group_name   = aws_db_parameter_group.education.name
+  publicly_accessible    = true
+  skip_final_snapshot    = true
 }
