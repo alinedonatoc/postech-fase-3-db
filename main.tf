38a8a892b292f07f77c7328a5ea15756978f03b2
aline.tanure <alinedonatoc@gmail.com>
2024-03-14 20:30:55 -0300

    Adjust db

--- main.tf
+++ main.tf
@@ -41,7 +41,6 @@ resource "aws_db_instance" "education" {
   engine_version         = "14.11"
   username               = "edu"
   password               = var.POSTGRES_DB_PASSWORD
-  db_subnet_group_name   = aws_db_subnet_group.education.name
   vpc_security_group_ids = [aws_security_group.rds.id]
   parameter_group_name   = aws_db_parameter_group.education.name
   publicly_accessible    = true
