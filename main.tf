353bcc7819fca5ff64461b31ee7275182beb3267
aline.tanure <alinedonatoc@gmail.com>
2024-03-16 13:59:51 -0300

    Change subnet

--- main.tf
+++ main.tf
@@ -68,6 +68,7 @@ resource "aws_db_instance" "education" {
   engine_version         = "14.11"
   username               = "education"
   password               = "education"
+  db_subnet_group_name   = aws_db_subnet_group.education.name
   vpc_security_group_ids = [aws_security_group.rds.id]
   parameter_group_name   = aws_db_parameter_group.education.name
   publicly_accessible    = true
